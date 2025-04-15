c1b5d4318d14a66c7a1552454c45e834a1a3c072
Hiten Mahajan <166706073+hiten016@users.noreply.github.com>
2025-04-16 03:53:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,30 +13,30 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    area = data['area']
-    bedrooms = data['bedrooms']
-    bathrooms = data['bathrooms']
-    stories = data['stories']
-    parking = data['parking']
-    furnishingstatus = data['furnishingstatus']
-    mainroad = data['mainroad']
-    guestroom = data['guestroom']
-    basement = data['basement']
-    hotwaterheating = data['hotwaterheating']
-    airconditioning = data['airconditioning']
+        area = float(data['area'])
+        bedrooms = float(data['bedrooms'])
+        bathrooms = float(data['bathrooms'])
+        stories = float(data['stories'])
+        parking = float(data['parking'])
+        furnishingstatus = data['furnishingstatus']
 
-    features = np.array([[area, bedrooms, bathrooms, stories, parking,
-                          furnishingstatus, mainroad, guestroom, basement, hotwaterheating, airconditioning]])
+        furnishingstatus_mapping = {'furnished': 1, 'semi-furnished': 2, 'unfurnished': 0}
+        furnishingstatus = furnishingstatus_mapping.get(furnishingstatus, 0)  
 
-    prediction = model.predict(features)
+        features = np.array([[area, bedrooms, bathrooms, stories, parking, furnishingstatus]])
 
-    predicted_price = np.expm1(prediction[0])
+        prediction = model.predict(features)
 
-    return jsonify({'predicted_price': predicted_price})
+        predicted_price = np.expm1(prediction[0])
+
+        return jsonify({'predicted_price': predicted_price})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-
     app.run(host="0.0.0.0", port=port, debug=True)
